be352373f97002fcaee116f814aa9c147ce10374
Flauz <dread94@gmail.com>
2022-12-05 14:46:35 +0100

    day4/part2

--- day4/index.js
+++ day4/index.js
@@ -2,7 +2,7 @@ import { input } from "./data.js"
 
 const pairs = input.split("\n");
 
-const sumOfPairs = () => {
+const sumOfPairsFullIncludeOrOverlap = (type) => {
     let sum = 0;
     pairs.forEach((pair) => {
         let [firstElf, secondElf] = pair.split(",");
@@ -14,9 +14,16 @@ const sumOfPairs = () => {
             sum += 1;
         } else if (firstElf[0] >= secondElf[0] && firstElf[1] <= secondElf[1]) {
             sum += 1;
+        } else if (2 === type) {
+            if (firstElf[0] <= secondElf[0] && secondElf[1] >= secondElf[0]) {
+                sum += 1;
+            } else if (firstElf[0] >= secondElf[1] && secondElf[1] <= secondElf[1]) {
+                sum += 1;
+            }
         }
     })
     return sum
 }
 
-console.log(sumOfPairs());
+console.log(sumOfPairsFullIncludeOrOverlap(1));
+console.log(sumOfPairsFullIncludeOrOverlap(2));
