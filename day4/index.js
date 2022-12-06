1c961013d2b3934f14f70f589753122ff87c4fd8
Flauz <dread94@gmail.com>
2022-12-06 11:15:29 +0100

    cleanup

--- day4/index.js
+++ day4/index.js
@@ -6,10 +6,8 @@ const sumOfPairsFullIncludeOrOverlap = (type) => {
     let sum = 0;
     pairs.forEach((pair) => {
         let [firstElf, secondElf] = pair.split(",");
-        firstElf = firstElf.split("-");
-        secondElf = secondElf.split("-");
-        firstElf = firstElf.map((item) => +item);
-        secondElf = secondElf.map((item) => +item);
+        firstElf = firstElf.split("-").map((item) => +item);
+        secondElf = secondElf.split("-").map((item) => +item);
         if (firstElf[0] <= secondElf[0] && firstElf[1] >= secondElf[1]) {
             sum += 1;
         } else if (firstElf[0] >= secondElf[0] && firstElf[1] <= secondElf[1]) {
